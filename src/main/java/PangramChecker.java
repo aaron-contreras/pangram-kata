213d24c6bf4a2a7236c81861ec406059e14ac585
Aaron Contreras <aaronlcaq@gmail.com>
2020-03-03 18:38:35 -0500

    Initial commit

---
+++ src/main/java/PangramChecker.java
@@ -0,0 +1,8 @@
+public class PangramChecker {
+
+    public boolean isPangram(String input) {
+
+            return input.toLowerCase().replaceAll("[^a-z]", "").chars().distinct().count() == 26;
+
+    }
+}
