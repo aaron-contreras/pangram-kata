ef20bf2c4df1de5745364cfc7bf625fba92bce72
Aaron Contreras <aaronlcaq@gmail.com>
2020-03-03 19:40:02 -0500

    Solution using a HashSet

--- src/main/java/PangramChecker.java
+++ src/main/java/PangramChecker.java
@@ -1,8 +1,21 @@
+import java.util.HashSet;
 public class PangramChecker {
+
+    String phrase;
 
     public boolean isPangram(String input) {
 
-            return input.toLowerCase().replaceAll("[^a-z]", "").chars().distinct().count() == 26;
+        if (input.isEmpty()) return false;
 
+        phrase = input.replaceAll("[^a-zA-Z]", "").toLowerCase();
+
+        HashSet<Character> characters = new HashSet<Character>();
+
+        for (int i = 0; i<phrase.length(); i++) {
+            char currentChar = phrase.charAt(i);
+            characters.add(currentChar);
+        }
+
+        return characters.size() == 26;
     }
 }
